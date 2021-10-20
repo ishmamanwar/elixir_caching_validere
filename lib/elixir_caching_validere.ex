1f02f9f753ebc953cafed53b5995675ba29a64c7
ishmam1 <ishmam@ezenroute.com>
2021-10-20 00:38:05 -0600

    initialize tables for caching

--- lib/elixir_caching_validere.ex
+++ lib/elixir_caching_validere.ex
@@ -12,6 +12,16 @@ defmodule ElixirCachingValidere do
       :world
 
   """
+
+  use GenServer
+
+  defstruct table: nil, ttl_table: nil, size: 0, evict_fn: nil
+
+  def start_link(name, size, opts \\ []) do
+    Agent.start_link(__MODULE__, :init, [name, size, opts], name: name)
+  end
+
+
   def hello do
     :world
   end
