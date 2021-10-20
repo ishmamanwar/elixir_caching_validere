41acdc301ec608babb2388ddc5dbd77f7a601624
ishmam1 <ishmam@ezenroute.com>
2021-10-20 01:21:59 -0600

    handling get method and housekeeping

--- lib/elixir_caching_validere.ex
+++ lib/elixir_caching_validere.ex
@@ -1,32 +1,19 @@
 defmodule ElixirCachingValidere do
-  @moduledoc """
-  Documentation for `ElixirCachingValidere`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirCachingValidere.hello()
-      :world
-
-  """
 
   use GenServer
 
-  defstruct table: nil, ttl_table: nil, size: 0, evict_fn: nil
+  defstruct table: nil, time_table: nil, size: 0, evict_fn: nil
 
   def start_link(name, size, opts \\ []) do
     Agent.start_link(__MODULE__, :init, [name, size, opts], name: name)
   end
 
   def init(name, size, opts \\ []) do
-    ttl_table = :"#{name}_ttl"
-    :ets.new(ttl_table, [:named_table, :ordered_set])
+    time_table = :"#{name}_timetable"
+    :ets.new(time_table, [:named_table, :ordered_set])
     :ets.new(name, [:named_table, :public, {:read_concurrency, true}])
     evict_fn = Keyword.get(opts, :evict_fn)
-    %ElixirCachingValidere{ttl_table: ttl_table, table: name, size: size, evict_fn: evict_fn}
+    %ElixirCachingValidere{time_table: time_table, table: name, size: size, evict_fn: evict_fn}
   end
 
   def init({name, size, opts}) do
@@ -37,34 +24,34 @@ defmodule ElixirCachingValidere do
     do: Agent.get(name, __MODULE__, :handle_put, [key, value])
 
   def handle_put(state = %{table: table}, key, value) do
-    delete_ttl(state, key)
-    uniq = insert_ttl(state, key)
+    delete_timetable(state, key)
+    uniq = insert_timetable(state, key)
     :ets.insert(table, {key, uniq, value})
-    clean_oversize(state)
+    reset_values(state)
     :ok
   end
 
-  defp delete_ttl(%{ttl_table: ttl_table, table: table}, key) do
+  defp delete_timetable(%{time_table: time_table, table: table}, key) do
     case :ets.lookup(table, key) do
       [{_, old_uniq, _}] ->
-        :ets.delete(ttl_table, old_uniq)
+        :ets.delete(time_table, old_uniq)
 
       _ ->
         nil
     end
   end
 
-  defp insert_ttl(%{ttl_table: ttl_table}, key) do
-    uniq = :erlang.unique_integer([:monotonic])
-    :ets.insert(ttl_table, {uniq, key})
-    uniq
+  defp insert_timetable(%{time_table: time_table}, key) do
+    position = :erlang.unique_integer([:monotonic])
+    :ets.insert(time_table, {position, key})
+    position
   end
 
-  defp clean_oversize(state = %{ttl_table: ttl_table, table: table, size: size}) do
+  defp reset_values(state = %{time_table: time_table, table: table, size: size}) do
     if :ets.info(table, :size) > size do
-      oldest_tstamp = :ets.first(ttl_table)
-      [{_, old_key}] = :ets.lookup(ttl_table, oldest_tstamp)
-      :ets.delete(ttl_table, oldest_tstamp)
+      oldest_tstamp = :ets.first(time_table)
+      [{_, old_key}] = :ets.lookup(time_table, oldest_tstamp)
+      :ets.delete(time_table, oldest_tstamp)
       call_evict_fn(state, old_key)
       :ets.delete(table, old_key)
       true
@@ -80,6 +67,23 @@ defmodule ElixirCachingValidere do
     evict_fn.(key, value)
   end
 
+  def get(name, key, touch \\ true) do
+    case :ets.lookup(name, key) do
+      [{_, _, value}] ->
+        touch && Agent.get(name, __MODULE__, :handle_touch, [key])
+        value
+
+      [] ->
+        nil
+    end
+  end
+
+  def handle_touch(state = %{table: table}, key) do
+    delete_timetable(state, key)
+    uniq = insert_timetable(state, key)
+    :ets.update_element(table, key, [{2, uniq}])
+    :ok
+  end
 
   def hello do
     :world
