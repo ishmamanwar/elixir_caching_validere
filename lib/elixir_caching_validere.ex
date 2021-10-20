029fd084e74e07c0f9b2db9658f3eb14564e4d58
ishmam1 <ishmam@ezenroute.com>
2021-10-20 00:29:07 -0600

    Initial commit

---
+++ lib/elixir_caching_validere.ex
@@ -0,0 +1,18 @@
+defmodule ElixirCachingValidere do
+  @moduledoc """
+  Documentation for `ElixirCachingValidere`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirCachingValidere.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_caching_validere_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirCachingValidereTest do
+  use ExUnit.Case
+  doctest ElixirCachingValidere
+
+  test "greets the world" do
+    assert ElixirCachingValidere.hello() == :world
+  end
+end
