ac7d846f1efbe27643c93b10dcd9b0baef1755d4
ishmam1 <ishmam@ezenroute.com>
2021-10-21 00:38:03 -0600

    supervisor tree implemented

--- lib/elixir_caching_validere/web_server.ex
+++ lib/elixir_caching_validere/web_server.ex
@@ -7,7 +7,8 @@ defmodule ElixirCachingValidere.WebServer do
   end
 
   def call(conn, _opts) do
-    conn = put_resp_content_type(conn, "text/plain")
-    send_resp(conn, 200, "Hello World")
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello World")
   end
 end
